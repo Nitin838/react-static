af3eaef7320a982a693f128d824ed4c7e0b1b654
Nitin838 <geetnitin838@gmail.com>
2022-06-03 13:13:20 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import reactLogo from "../../image/reactlogo.png";
+
+function Navbar() {
+    return (
+        <nav>
+            <img src={reactLogo} alt="React Logo" className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default Navbar;
